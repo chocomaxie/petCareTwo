9cdc81a1387a02a4cd3a5c0f295ca4b9d7e6a0fc
choy <choy@choys-MacBook-Pro.local>
2025-08-07 23:31:16 +0800

    first commit

--- src/components/Shelter.jsx
+++ src/components/Shelter.jsx
@@ -3,29 +3,29 @@ import React from 'react'
 const Shelter = () => {
   return (
     <div className='flex flex-col min-h-screen justify-center items-center'>
-      <div className="flex flex-col w-108 rounded-2xl bg-amber-500 p-5">
+      <div className="flex flex-col w-sm md:w-md rounded-2xl bg-amber-500 p-5">
         <div className="text-center">
-          <h1 className='text-3xl font-bold'>Animal Shelter Portal</h1>
-          <p>Create your animal shelter account</p>
+          <h1 className='font-bold text-2xl md:text-3xl'>Animal Shelter Portal</h1>
+          <p className='text-[14px] md:text-[17px]'>Create your animal shelter account</p>
         </div>
         <form>
         <div className="mt-2 mb-2">
-          <label className='block mb-1' htmlFor="name">Full name</label>
-          <input className='w-full p-2 border' type="text" placeholder='Enter your full name' name='name' id='name' autoComplete='off' required/>
+          <label className='block mb-1 text-[14px] md:text-[17px]' htmlFor="name">Full name</label>
+          <input className='w-full p-2 border text-[14px] md:text-[17px]' type="text" placeholder='Enter your full name' name='name' id='name' autoComplete='off' required/>
         </div>
         <div className="mb-2">
-          <label className='block mb-1' htmlFor="email">Email</label>
-          <input className='w-full p-2 border' type="email" placeholder='Enter your email' name='email' id='email' autoComplete='off' required/>
+          <label className='block mb-1 text-[14px] md:text-[17px]' htmlFor="email">Email</label>
+          <input className='w-full p-2 border text-[14px] md:text-[17px]' type="email" placeholder='Enter your email' name='email' id='email' autoComplete='off' required/>
         </div>
         <div className="mb-2">
-          <label className='block mb-1' htmlFor="password">Password</label>
-          <input className='w-full p-2 border' type="password" placeholder='Enter your password' name='password' id='password' maxLength={12} required/>
+          <label className='block mb-1 text-[14px] md:text-[17px]' htmlFor="password">Password</label>
+          <input className='w-full p-2 border text-[14px] md:text-[17px]' type="password" placeholder='Enter your password' name='password' id='password' maxLength={12} required/>
         </div>
         <div className="mb-2">
-          <label className='block mb-1' htmlFor="confirm">Confirm Password</label>
-          <input className='w-full p-2 border' type="password" placeholder='Confirm your password' name='confirm' id='confirm' required />
+          <label className='block mb-1 text-[14px] md:text-[17px]' htmlFor="confirm">Confirm Password</label>
+          <input className='w-full p-2 border text-[14px] md:text-[17px]' type="password" placeholder='Confirm your password' name='confirm' id='confirm' required />
         </div>
-        <button className='bg-white mt-2 p-2 rounded-2xl cursor-pointer transition-all hover:bg-green-300 w-full'>Create account</button>
+        <button className='bg-white mt-2 p-2 rounded-2xl text-[14px] md:text-[17px] font-semibold cursor-pointer transition-all hover:bg-green-300 w-full'>Create account</button>
         </form>
       </div>
     </div>
