fd390f9882218e5b1621421b6a5212c7ab024e2f
choy <choy@choys-MacBook-Pro.local>
2025-08-08 09:58:17 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import FirstPage from './Firstpage';
+import Firstpage from './Firstpage';
 import Shelter from './components/Shelter';
 import Home from './components/Home';
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<FirstPage />} />
+        <Route path='/' element={<Firstpage />} />
         <Route path='shelter' element={<Shelter />} />
         <Route path='home' element={<Home />} />
       </Routes>
